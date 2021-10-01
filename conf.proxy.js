8cc35d552a30874f52ff9384f8571883fce4c690
cenfun <cenfun@qq.com>
2021-10-01 15:00:51 +0800

    update proxy

--- conf.proxy.js
+++ conf.proxy.js
@@ -2,41 +2,21 @@ module.exports = {
     create: function(option) {
         const service = {
 
-            /*
-            //proxy example:
-            "/api": {
-                DEV: {
-                    target: '',
+            default: {
+                "/api": {
+                    target: "http://localhost:8080",
                     pathRewrite: {
-                        '^/api/': '/service/'
-                    },
-                    headers: {
-                    
-                    }
-                },
-                QA: {
-                    target: '',
-                    headers: {
-                    
-                    }
-                },
-                STG: {
-                    target: '',
-                    headers: {
-        
-                    }
-                },
-                LOCAL: {
-                    target: 'http://localhost:8080',
-                    pathRewrite: {
-                        '^/api/': '/'
+                        "^/api/": "/"
                     },
                     headers: {
                         
                     }
                 }
-            }
-            */
+            },
+
+            dev: {},
+            qa: {},
+            stg: {}
 
         };
 
