d610443362d18b6cdfff9d8f0b6324a42e731a96
cenfun <cenfun@qq.com>
2022-05-28 10:38:38 +0800

    update build env and define

--- conf.cli.js
+++ conf.cli.js
@@ -90,6 +90,11 @@ module.exports = {
     },
 
     build: {
+        //for DefinePlugin
+        define: {
+            __VUE_OPTIONS_API__: true,
+            __VUE_PROD_DEVTOOLS__: false
+        },
         //for flatdep
         exclude: [],
         include: [],
